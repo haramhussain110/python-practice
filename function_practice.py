fc02f8b319305a54e35ab4501855e1a0dd42a865
Haram Hussain <haramchohan928@gmail.com>
2025-09-02 16:55:31 +0500

    Add Python functions practice file
    This commit adds a functions practice program in Python:
    - greet() → prints a string multiple times
    - name(name) →  prints a personalized greeting
    - welcome(name, city) → shows default parameters usage
    - g() → prints a global variable

---
+++ function_practice.py
@@ -0,0 +1,21 @@
+def greet():
+    v ="Haram Chohan " 
+    print(v*100 , end ="" )
+
+def name(name ):
+    print("hi",name)
+
+    
+def welcome(name = "haram" ,city ="hyd"):
+    print(f'{name} from {city}')
+
+welcome(name = "zain" ,city ="karachi")
+
+
+
+
+choice = "my love"
+def g():
+     print(f'{choice}')
+    
+g()
